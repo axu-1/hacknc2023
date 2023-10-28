f28e436bf7d691a8369c4f379a71a4a4c73986ed
kobwebby <knightofbirds@gmail.com>
2023-10-28 18:50:00 -0400

    I'm sleepy

--- hacknc2023-app/App.js
+++ hacknc2023-app/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export default function App() {
 
@@ -10,6 +11,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style ={{color: '#fff', marginBottom: 10}} >Welcome to the password cracking game!</Text>
+      <Text style ={{color: '#fff', marginBottom: 10}} >To begin, start by entering a fake username and password.</Text>
       <StatusBar style="auto" />
       <TextInput
         style={TextBoxStyles.input}
@@ -24,16 +26,35 @@ export default function App() {
         value={password}
         onChangeText={text => setPassword(text)}
       />
+      <View style={styles.bottomContainer}>
+        <View style={styles.border}>
+          <Text style={styles.bottomText}>IMPORTANT: Do not use a password that you actually use in real life, especially if it's for a sensitive application!</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
+//STYLES GO HERE UNTIL YOU CAN FIGURE OUT INTER FILE NAVIGATION
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bottomContainer: {
+    //flex: 1,
+    backgroundColor: '#25292e',
+    borderColor: 'red',
+    borderWidth: 2,
+    borderRadius: 4,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  bottomText: {
+    color: 'white',
+    padding: 20
   },
 });
 
@@ -54,8 +75,7 @@ const TextBoxStyles = StyleSheet.create({
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 10,
-    // Highlight effect when focused
-    shadowColor: 'blue', // Change to your desired highlight color
+    shadowColor: 'blue',
     shadowOpacity: 0.5,
     shadowRadius: 5,
     shadowOffset: {
