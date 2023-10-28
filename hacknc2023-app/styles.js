244d862ef7695f7eae977ce9dbf3c62b1f642025
kobwebby <knightofbirds@gmail.com>
2023-10-28 17:19:13 -0400

    Just kidding reworked for expo

---
+++ hacknc2023-app/styles.js
@@ -0,0 +1,18 @@
+//For all the styles that will be used in this app
+/* example
+//myStyles.js
+export const MyStyles = StyleSheet.create({
+  redText: {
+    color: 'red',
+  },
+});
+*/
+
+/* 
+import {MyStyles} from "/path/to/myStyles"
+
+//...other code
+<Text style={MyStyles.redText}>this is red </Text>
+//... other code
+
+*/
